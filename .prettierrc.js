ac1e9ed175d1fe69177ca83c70dc8abc3ea19ab0
Manu <yoan85@gmail.com>
2022-01-19 06:57:57 -0500

    feat(arch): added eslint rules, prettier and commitizen

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true,
+  bracketSpacing: true,
+  trailingComma: 'none',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  arrowParens: 'avoid',
+  jsxBracketSameLine: true,
+  endOfLine: 'lf'
+};
+
