299138991adbdc1b751a46f2b553064579834dc8
Manu <manujsdev@gmail.com>
2022-03-18 14:33:03 -0400

    feat(google analytics): added google analytics feature

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,30 +1,46 @@
-import Document from 'next/document'
+import Document from 'next/document';
+import Script from 'next/script';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App: any) => (props: any) =>
-            sheet.collectStyles(<App {...props} />),
-        })
+          enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />)
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
           <>
+            <Script
+              strategy="lazyOnload"
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            />
+            <Script strategy="lazyOnload">
+              {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}, {
+              page_path: window.location.pathname,
+            });
+        `}
+            </Script>
+
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        ),
-      }
+        )
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
 }
