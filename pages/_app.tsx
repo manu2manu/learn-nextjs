b9521c02412e47a4156ecd9b8dc0fe6742c1a17d
Manu <manujsdev@gmail.com>
2022-03-19 19:13:46 -0400

    Revert "feat(google analytics): added google analytics feature"
    This reverts commit 299138991adbdc1b751a46f2b553064579834dc8.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,9 @@
 import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 import { DrawerContext } from '../components/contexts/DrawerContext';
 import { ThemeProviderContext } from '../components/contexts/ThemeContext';
 import { ThemeProviderStyled } from '../components/contexts/ThemeProviderStyled';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      (window as any).gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-        page_path: url
-      });
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
   return (
     <ThemeProviderContext>
       <ThemeProviderStyled>
