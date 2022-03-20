772d18ab55acc3373498fd83cc087b1f0ae6ac14
Manu <manujsdev@gmail.com>
2022-03-19 21:37:53 -0400

    feat(ga): added google analytics feature

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,55 @@
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import { useEffect } from 'react';
 import { DrawerContext } from '../components/contexts/DrawerContext';
 import { ThemeProviderContext } from '../components/contexts/ThemeContext';
 import { ThemeProviderStyled } from '../components/contexts/ThemeProviderStyled';
+import { pageview } from '../lib/ga';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  console.log('env: ', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <ThemeProviderContext>
-      <ThemeProviderStyled>
-        <DrawerContext>
-          <Component {...pageProps} />
-        </DrawerContext>
-      </ThemeProviderStyled>
-    </ThemeProviderContext>
+    <>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics-script" strategy="afterInteractive">
+        {` 
+         window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+      <ThemeProviderContext>
+        <ThemeProviderStyled>
+          <DrawerContext>
+            <Component {...pageProps} />
+          </DrawerContext>
+        </ThemeProviderStyled>
+      </ThemeProviderContext>
+    </>
   );
 }
 
